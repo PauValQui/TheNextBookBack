acf51bb2d29be485a9c0df509f9479cf71a3b0d9
PauValQui <paulavq103@gmail.com>
2022-05-25 10:09:44 +0200

    Llamada a los Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CategoriaSeeder::class);
+        $this->call(AutorSeeder::class);
+        $this->call(LibroSeeder::class);
     }
 }
